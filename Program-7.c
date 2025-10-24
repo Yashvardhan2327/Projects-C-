3d53882e49c1f58a9850b79692a1c8c347446840
Yashvardhan2327 <yashvardhan.chandel5461@gmail.com>
2025-10-24 13:23:22 +0530

    Create Program-7.c

---
+++ Program-7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    printf("Enter your age : ");
+    scanf("%d", &age);
+
+    if (age>=18) {
+        printf("Eligible to vote.\n");
+    }
+    else{
+        if (age<0){
+            printf("Invalid age. \n");
+
+        }
+        {
+            printf("Not eligible to vote.\n");
+        }
+        return 0;
+    }
+}
